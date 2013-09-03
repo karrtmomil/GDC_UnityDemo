cc1a7d69c944ab8fdfc354e18fd8d9c6cabc8c72
Morgan <morgan.hoke@gmail.com>
2013-09-03 13:59:06 -0500

    added shooting stuffs

---
+++ GDCDemoProject/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+	
+	public void initBullet(string Direction, Transform InitialPosition)
+	{
+		if(Direction.Equals ("north",System.StringComparison.InvariantCultureIgnoreCase))
+		{
+		}
+		else if (Direction.Equals ("south",System.StringComparison.InvariantCultureIgnoreCase))
+		{
+		}
+		else if (Direction.Equals ("east",System.StringComparison.InvariantCultureIgnoreCase))
+		{
+		}
+		else if (Direction.Equals ("west",System.StringComparison.InvariantCultureIgnoreCase))
+		{
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GDCDemoProject/Assets/Scripts/ShooterScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShooterScript : MonoBehaviour {
+	public Transform Bullet = null;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 FireDirection = new Vector2(Input.GetAxis("FireHorizontal"), Input.GetAxis("FireVertical"));
+		
+		// We use square magnitude because it is more efficient than square rooting plus we are only checking for non-zero here
+		if(FireDirection.sqrMagnitude > 0)
+		{
+			GameObject newBullet = Instantiate(Bullet, transform.position, Quaternion.identity) as GameObject;
+			newBullet.GetComponent<BulletScript>().initBullet("",transform);
+		}
+	}
+}
